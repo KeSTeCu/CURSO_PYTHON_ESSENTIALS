4034f460d7e3ba03dba7252f023178ef68526ce0
KeSTeCu <144753368+KeSTeCu@users.noreply.github.com>
2023-09-11 21:10:09 -0500

    Add files via upload

---
+++ 03_personal-info.py
@@ -0,0 +1,11 @@
+
+
+nombre=input('INGRESE SU NOMBRE: ')
+apellido=input('Ingrese su aprellido: ')
+locacion=input('Ingrese su locacion: ')
+edad=int(input('Ingrese su edad: '))
+
+print('era una vez un unicornio tuerto llamado ', nombre,'de la tribu ', apellido, ', ubicado en la no tan magica tierra de ', locacion,' en esta tribu existe una costumbre la cual era que a la tierna edad de ',edad,' años debian domar un capibara')
+
+
+
